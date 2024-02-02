8b20ace2ffbbece76e635685ddaaa2666a3ed3a9
hanane987 <144786085+hanane987@users.noreply.github.com>
2024-02-02 22:27:47 +0100

    Add files via upload

---
+++ database/migrations/2024_01_30_173337_create_recipes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',80);
+            $table->longText('description');
+            $table->string('image_path')->nullable();
+            $table->foreignIdFor(User::class,'users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
---
+++ database/migrations/2024_02_02_125018_add_foriegn_key.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('recipes', function (Blueprint $table) {
+
+            $table->foreignIdFor(User::class, 'user_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('recipes', function (Blueprint $table) {
+
+            $table->dropforeignIdFor(User::class, 'user_id');
+
+        });
+    }
+};
